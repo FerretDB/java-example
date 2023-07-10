474c5700835c10011df05b2dbc2a3216d7bf0711
b1ron <goldingbyron@gmail.com>
2023-07-10 21:02:48 +0200

    tidy

--- java/src/main/java/com/start/Connection.java
+++ java/src/main/java/com/start/Connection.java
@@ -2,9 +2,6 @@ package com.start;
 
 import org.bson.Document;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoException;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
@@ -12,19 +9,13 @@ import com.mongodb.client.MongoDatabase;
 public class Connection {
 
     public static void main(String[] args) {
-        for (String connectionUri : args) {
-            try (MongoClient mongoClient = MongoClients.create(connectionUri)) {
-                MongoDatabase database = mongoClient.getDatabase("test");
+        try (MongoClient mongoClient = MongoClients.create(args[0])) {
+            MongoDatabase database = mongoClient.getDatabase("test");
 
-                try {
-                    Document command = new Document("ping", 1);
-                    Document res = database.runCommand(command);
-                    System.out.println(res);
-                } catch (MongoException e) {
-                    System.err.println("An error occurred: " + e);
-                }
+            Document command = new Document("ping", 1);
+            Document res = database.runCommand(command);
 
-            }
+            System.out.println(res.toJson());
         }
     }
 }

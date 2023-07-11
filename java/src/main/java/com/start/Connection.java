6b9377f7414722a700d9d3ba0b520e6843b4d627
b1ron <80292536+b1ron@users.noreply.github.com>
2023-07-11 16:06:14 +0200

    Parse URI from args (#2)
    Closes FerretDB/dance#479.

--- java/src/main/java/com/start/Connection.java
+++ java/src/main/java/com/start/Connection.java
@@ -1,19 +1,22 @@
 package com.start;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import org.bson.Document;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 
 public class Connection {
 
     public static void main(String[] args) {
-        String connectionString = "mongodb://localhost:27017";
-        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
-            List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
-            databases.forEach(db -> System.out.println(db.toJson()));
+        try (MongoClient mongoClient = MongoClients.create(args[0])) {
+            MongoDatabase database = mongoClient.getDatabase("test");
+
+            Document command = new Document("ping", 1);
+            Document res = database.runCommand(command);
+
+            System.out.println(res.toJson());
+            mongoClient.close();
         }
     }
 }

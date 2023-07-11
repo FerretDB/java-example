7d080342f6681708467ff40c13b1748a3abd051c
b1ron <goldingbyron@gmail.com>
2023-07-11 15:03:50 +0200

    exit status 2

--- java/src/main/java/com/start/Connection.java
+++ java/src/main/java/com/start/Connection.java
@@ -9,6 +9,10 @@ import com.mongodb.client.MongoDatabase;
 public class Connection {
 
     public static void main(String[] args) {
+        if (args.length > 1) {
+            System.err.println("too many arguments");
+            System.exit(2);
+        }
         try (MongoClient mongoClient = MongoClients.create(args[0])) {
             MongoDatabase database = mongoClient.getDatabase("test");
 
@@ -16,6 +20,7 @@ public class Connection {
             Document res = database.runCommand(command);
 
             System.out.println(res.toJson());
+            mongoClient.close();
         }
     }
 }

4bd6370fa51b2048a55bbdfeaba8bc456cd42df0
b1ron <goldingbyron@gmail.com>
2023-07-28 12:56:32 +0200

    add finally statement and resolve imports

--- java/src/main/java/com/start/Connection.java
+++ java/src/main/java/com/start/Connection.java
@@ -7,15 +7,22 @@ import org.bson.Document;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.ConnectionString;
 
 public class Connection {
 
     public static void main(String[] args) {
+        MongoClient mongoClient;
+
         // if set in the YAML file
         if (args[args.length-1] == "StableAPI") {
-            uri = args[args.length-2];
+            String uri = args[args.length-2];
 
-            MongoClient client = MongoClients.create(
+            mongoClient = MongoClients.create(
             MongoClientSettings.builder()
             .applyConnectionString(new ConnectionString(uri))
             .serverApi(
@@ -25,8 +32,10 @@ public class Connection {
                 .build()
                 ).build()
             );
+        } else {
+            mongoClient = MongoClients.create(args[args.length-1]);
         }
-        try (MongoClient mongoClient = MongoClients.create(args[0])) {
+        try {
             MongoDatabase database = mongoClient.getDatabase("test");
 
             Document command = new Document("ping", 1);
@@ -40,10 +49,10 @@ public class Connection {
             System.out.println(res);
 
             ArrayList docList = new ArrayList();
-            for (var i = 1; i < 5; i++) {
+            for (int i = 1; i < 5; i++) {
                 BasicDBObject doc = new BasicDBObject("_id", i);
                 doc.append("a", i);
-                docList.append(doc);
+                docList.add(doc);
             }
 
             command = new Document("insert", docList);
@@ -51,13 +60,15 @@ public class Connection {
 
             System.out.println(res);
 
-            doc = new BasicDBObject("a", 4);
+            Document doc = new Document("a", 4);
             command = new Document("find", doc);
             res = database.runCommand(command);
 
             assert res == doc;
 
             mongoClient.close();
+        } finally {
+            // ...
         }
     }
 }

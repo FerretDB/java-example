cffd77266032702a920d2b4214eff6a0dcd3cdfe
b1ron <goldingbyron@gmail.com>
2023-07-28 12:38:19 +0200

    add commands

--- java/src/main/java/com/start/Connection.java
+++ java/src/main/java/com/start/Connection.java
@@ -1,5 +1,7 @@
 package com.start;
 
+import java.util.ArrayList;
+
 import org.bson.Document;
 
 import com.mongodb.client.MongoClient;
@@ -9,6 +11,21 @@ import com.mongodb.client.MongoDatabase;
 public class Connection {
 
     public static void main(String[] args) {
+        // if set in the YAML file
+        if (args[args.length-1] == "StableAPI") {
+            uri = args[args.length-2];
+
+            MongoClient client = MongoClients.create(
+            MongoClientSettings.builder()
+            .applyConnectionString(new ConnectionString(uri))
+            .serverApi(
+                ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .strict(false)
+                .build()
+                ).build()
+            );
+        }
         try (MongoClient mongoClient = MongoClients.create(args[0])) {
             MongoDatabase database = mongoClient.getDatabase("test");
 
@@ -16,6 +33,20 @@ public class Connection {
             Document res = database.runCommand(command);
 
             System.out.println(res.toJson());
+
+            command = new Document("dropDatabase", "test");
+            res = database.runCommand(command);
+
+            ArrayList docList = new ArrayList();
+            for (var i = 1; i < 5; i++) {
+                BasicDBObject doc = new BasicDBObject("_id", i);
+                doc.append("a", i);
+                docList.append(doc);
+            }
+
+            command = new Document("insert", docList);
+            res = database.runCommand(command);
+
             mongoClient.close();
         }
     }

5b2e50a270476dd74e634d605bfdec793fd21307
b1ron <goldingbyron@gmail.com>
2023-08-07 12:49:23 +0200

    remove stable API settings

--- java/src/main/java/com/start/Connection.java
+++ java/src/main/java/com/start/Connection.java
@@ -1,78 +1,52 @@
 package com.start;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.bson.Document;
+
+import static com.mongodb.client.model.Filters.eq;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.result.InsertManyResult;
-import com.mongodb.BasicDBObject;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.ServerApi;
-import com.mongodb.ServerApiVersion;
-import com.mongodb.ConnectionString;
 
 public class Connection {
 
     public static void main(String[] args) {
         MongoClient mongoClient;
+        mongoClient = MongoClients.create(args[args.length-1]);
 
-        // if set in the YAML file
-        if (args[args.length-1] == "StableAPI") {
-            String uri = args[args.length-2];
+        MongoDatabase database = mongoClient.getDatabase("test");
 
-            mongoClient = MongoClients.create(
-            MongoClientSettings.builder()
-            .applyConnectionString(new ConnectionString(uri))
-            .serverApi(
-                ServerApi.builder()
-                .version(ServerApiVersion.V1)
-                .strict(false)
-                .build()
-                ).build()
-            );
-        } else {
-            mongoClient = MongoClients.create(args[args.length-1]);
+        Document command = new Document("ping", 1);
+        Document res = database.runCommand(command);
+
+        System.out.println(res.toJson());
+
+        command = new Document("dropDatabase", 1);
+        res = database.runCommand(command);
+
+        System.out.println(res.toJson());
+
+        List<Document> docList = new ArrayList<Document>(4);
+        for (int i = 1; i < 5; i++) {
+            Document doc = new Document("_id", i);
+            doc.append("a", i);
+            docList.add(doc);
         }
-        try {
-            MongoDatabase database = mongoClient.getDatabase("test");
 
-            Document command = new Document("ping", 1);
-            Document res = database.runCommand(command);
+        MongoCollection<Document> collection = database.getCollection("foo");
+        InsertManyResult imres = collection.insertMany(docList);
 
-            System.out.println(res.toJson());
+        System.out.println(imres.toString());
 
-            command = new Document("dropDatabase", "test");
-            res = database.runCommand(command);
+        Document doc = collection.find(eq("a", 4)).first();
 
-            System.out.println(res.toJson());
+        assert res == doc;
 
-            List<Document> docList = Arrays.asList();
-            for (int i = 1; i < 5; i++) {
-                Document doc = new Document("_id", i);
-                doc.append("a", i);
-                docList.add(i, doc);
-            }
-
-            MongoCollection<Document> collection = database.getCollection("foo");
-            InsertManyResult imres = collection.insertMany(docList, null);
-
-            System.out.println(imres.toString());
-
-            Document doc = new Document("a", 4);
-            command = new Document("find", doc);
-            res = database.runCommand(command);
-
-            assert res == doc;
-
-            mongoClient.close();
-        } finally {
-            // ...
-        }
+        mongoClient.close();
     }
 }

3d0b6cbd0338fb7690b8969e6fe8cd5672738f0c
b1ron <80292536+b1ron@users.noreply.github.com>
2023-08-10 14:55:38 +0200

    Extend functionality of the java-example (#3)
    Closes FerretDB/dance#530.

--- java/src/main/java/com/start/Connection.java
+++ java/src/main/java/com/start/Connection.java
@@ -1,22 +1,48 @@
 package com.start;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bson.Document;
+
+import static com.mongodb.client.model.Filters.eq;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.InsertManyResult;
 
 public class Connection {
 
     public static void main(String[] args) {
-        try (MongoClient mongoClient = MongoClients.create(args[0])) {
-            MongoDatabase database = mongoClient.getDatabase("test");
+        MongoClient mongoClient;
+        mongoClient = MongoClients.create(args[args.length-1]);
 
-            Document command = new Document("ping", 1);
-            Document res = database.runCommand(command);
+        MongoDatabase database = mongoClient.getDatabase("test");
 
-            System.out.println(res.toJson());
-            mongoClient.close();
-        }
+        Document command = new Document("ping", 1);
+        Document res = database.runCommand(command);
+
+        assert res.equals(new Document("ok", 1.0)) : "ping failed";
+
+        command = new Document("dropDatabase", 1);
+        res = database.runCommand(command);
+
+        assert res.equals(new Document("ok", 1.0)) : "dropDatabase failed";
+
+        List<Document> docList = new ArrayList<Document>(4);
+        docList.add(new Document("_id", 1).append("a", 1));
+        docList.add(new Document("_id", 2).append("a", 2));
+        docList.add(new Document("_id", 3).append("a", 3));
+        docList.add(new Document("_id", 4).append("a", 4));
+
+        MongoCollection<Document> collection = database.getCollection("foo");
+        collection.insertMany(docList);
+
+        Document actual = collection.find(eq("a", 4)).first();
+        assert actual.equals(new Document("_id", 4).append("a", 4)) : "Value should be 4";
+
+        mongoClient.close();
     }
 }

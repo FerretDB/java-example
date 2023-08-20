14a406ce4a3e92c835e15ec3a3971b7e4053a5a4
b1ron <80292536+b1ron@users.noreply.github.com>
2023-08-20 20:34:32 +0200

    Revert "fix (#4)" (#5)
    This reverts commit b16768ea5e71ffb2c61da606234827d549e3b843.

--- java/src/main/java/com/start/Connection.java
+++ java/src/main/java/com/start/Connection.java
@@ -11,6 +11,7 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.InsertManyResult;
 
 public class Connection {
 
@@ -28,7 +29,7 @@ public class Connection {
         command = new Document("dropDatabase", 1);
         res = database.runCommand(command);
 
-        assert res.equals(new Document("dropped", "test").append("ok", 1.0)) : "dropDatabase failed";
+        assert res.equals(new Document("ok", 1.0)) : "dropDatabase failed";
 
         List<Document> docList = new ArrayList<Document>(4);
         docList.add(new Document("_id", 1).append("a", 1));

9ff08cdc1545b27352ed929c39e55ddd5ee5b1a8
b1ron <80292536+b1ron@users.noreply.github.com>
2023-11-21 14:04:39 +0100

    fix assertions (#6)
    Closes FerretDB/dance#651.

--- java/src/main/java/com/start/Connection.java
+++ java/src/main/java/com/start/Connection.java
@@ -11,7 +11,6 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.result.InsertManyResult;
 
 public class Connection {
 
@@ -24,12 +23,12 @@ public class Connection {
         Document command = new Document("ping", 1);
         Document res = database.runCommand(command);
 
-        assert res.equals(new Document("ok", 1.0)) : "ping failed";
+        assert res.getDouble("ok").equals(1.0) : "ping failed";
 
         command = new Document("dropDatabase", 1);
         res = database.runCommand(command);
 
-        assert res.equals(new Document("ok", 1.0)) : "dropDatabase failed";
+        assert res.getDouble("ok").equals(1.0) : "dropDatabase failed";
 
         List<Document> docList = new ArrayList<Document>(4);
         docList.add(new Document("_id", 1).append("a", 1));
